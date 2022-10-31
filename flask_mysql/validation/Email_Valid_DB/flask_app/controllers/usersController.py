292e714270147a5631d858802c2ff83f3e407abb
Christopher Meehan <chrismeehan91@gmail.com>
2022-10-31 15:41:54 -0500

    updated validations

--- flask_mysql/validation/Email_Valid_DB/flask_app/controllers/usersController.py
+++ flask_mysql/validation/Email_Valid_DB/flask_app/controllers/usersController.py
@@ -4,6 +4,7 @@ from flask import render_template, redirect, request, session
 
 @app.route('/')
 def users():
+    session.clear()
     return render_template('users.html', users = User.get_all())
 
 @app.route('/newuser')
@@ -12,7 +13,16 @@ def newuser():
 
 @app.route('/create', methods=['POST'])
 def save():
-    if not User.validate_user(request.form):
+    email_in_db = User.check_emails(request.form['email'])
+    if email_in_db:
+        email_is_unique = False
+    else:
+        email_is_unique = True
+
+    if not User.validate_user(request.form, email_is_unique):
+        session["first_name"] = request.form["first_name"]
+        session["last_name"] = request.form["last_name"]
+        session["email"] = request.form["email"]
         return redirect('/newuser')
     data = {
         "first_name": request.form["first_name"],
@@ -20,6 +30,7 @@ def save():
         "email": request.form["email"]
     }
     id = User.save(data)
+    session.clear()
     return redirect(f'/show/{id}')
     # data = {request.form}
     # User.save(data)
@@ -33,19 +44,30 @@ def show(id):
 @app.route('/edit/<int:id>')
 def edit(id):
     data = {'id': id}
-    return render_template('edit.html', user = User.get_one(data))
+    user = User.get_one(data)
+    if "first_name" not in session:
+        session['first_name'] = user.first_name 
+    if "last_name" not in session:
+        session['last_name'] = user.last_name
+    return render_template('edit.html', user = user)
 
 @app.route('/update/<int:id>', methods=['POST'])
 def update(id):
-    if not User.validate_user(request.form):
-        return redirect(f'/edit/{id}')
     data = {
         'id': id,
         'first_name': request.form['first_name'],
         'last_name': request.form['last_name'],
-        'email': request.form['email']
+        # 'email': request.form['email']
     }
+    if not User.validate_user_update(request.form):
+        session["first_name"] = request.form["first_name"]
+        session["last_name"] = request.form["last_name"]
+        # session["email"] = request.form["email"]
+        return redirect(f'/edit/{id}')
+    print("B")
     user = User.update(data)
+    session.pop('first_name')
+    session.pop('last_name')
     return redirect(f'/show/{id}')
 
 @app.route('/delete/<int:id>')
--- flask_mysql/validation/Email_Valid_DB/flask_app/models/users.py
+++ flask_mysql/validation/Email_Valid_DB/flask_app/models/users.py
@@ -28,7 +28,7 @@ class User:
 
     @classmethod
     def update(cls, data):
-        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;"
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s WHERE id = %(id)s;"
         return connectToMySQL("user_crud_2_schema").query_db(query, data)
 
     @classmethod
@@ -43,8 +43,17 @@ class User:
         user = cls(results[0])
         return user
 
+    @classmethod
+    def check_emails(cls, data):
+        query = "SELECT email FROM users WHERE email = %(email)s;"
+        data = {
+            "email": data
+        }
+        result = connectToMySQL('user_crud_2_schema').query_db(query, data)
+        return result
+
     @staticmethod
-    def validate_user(user):
+    def validate_user(user, email_is_unique):
         is_valid = True
         if not user['first_name'].isalpha():
             flash("Must fill out field")
@@ -52,8 +61,22 @@ class User:
         if len(user['last_name']) <= 0:
             flash("Must fill out field")
             is_valid = False
+        if not email_is_unique:
+            flash("Duplicate Email not allowed")
+            is_valid = False
         if not EMAIL_REGEX.match(user['email']):
             flash("Invalid email address!")
+            is_valid = False
+        return is_valid
+
+    @staticmethod
+    def validate_user_update(user):
+        is_valid = True
+        if not user['first_name'].isalpha():
+            flash("Must fill out field")
+            is_valid = False
+        if len(user['last_name']) <= 0:
+            flash("Must fill out field")
             is_valid = False
         return is_valid
             
